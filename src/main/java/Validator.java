9898047738904d93c848b3eb32d0ec609679a36c
Stepan <stepan.stepanovich.16@gmail.com>
2019-12-30 17:17:49 +0200

    with parcer and trycatch

---
+++ src/main/java/Validator.java
@@ -0,0 +1,29 @@
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import java.io.File;
+import java.io.IOException;
+
+
+public class Validator extends DefaultHandler {
+
+    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+        SAXParserFactory factory = SAXParserFactory.newInstance();
+        SAXParser parser = factory.newSAXParser();
+
+        XMLHandler handler = new XMLHandler();
+        try {
+            parser.parse(new File(Validator.class.getClassLoader().getResource("dataset.xml").getFile()), handler);
+        }
+       catch (SAXException e){
+
+       }
+
+
+    }
+}
---
+++ src/main/java/XMLHandler.java
@@ -0,0 +1,66 @@
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import java.util.Stack;
+
+public class XMLHandler extends DefaultHandler {
+Stack<String> stack = new Stack<String>();
+
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+        /*if (qName.equals("user")) {
+            String firstName = attributes.getValue("firstName");
+            String lastName = attributes.getValue("lastName");
+            String age = attributes.getValue("age");
+            String email = attributes.getValue("email");
+            String technology = attributes.getValue("technology");
+            userList.add(new User(firstName, lastName, Integer.valueOf(age), email, technology, null));
+        } else if (qName.equals("admin")) {
+
+            String firstName = attributes.getValue("firstName");
+            String lastName = attributes.getValue("lastName");
+            String email = attributes.getValue("email");
+            String permission = attributes.getValue("permission");
+            adminList.add(new Admin(firstName, lastName, email, permission));
+        }*/
+        if(stack.empty()){
+            stack.push(qName);
+        }
+       else if(!qName.equals(stack.peek())){
+            stack.push(qName);
+
+    }
+
+    }
+
+    @Override
+    public void startDocument() throws SAXException {
+        // Тут будет логика реакции на начало документа
+    }
+
+    @Override
+    public void endDocument() throws SAXException {
+       if(stack.empty()) {
+           System.out.println("file is valid"); }
+       else{
+           System.out.println("file is INVALID!!!");
+       }
+    }
+
+
+    @Override
+    public void endElement(String uri, String localName, String qName) throws SAXException {
+       if(stack.empty()){
+           stack.push(qName);
+       }
+        else if(qName.equals(stack.peek())) {
+           stack.pop();
+       }
+        else{
+           stack.push(qName);
+       }
+    }
+}
+
+
